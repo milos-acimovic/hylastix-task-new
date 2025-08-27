9edbb5e64f41e74a667ea75a9cbbb1984ab3e178
milos-acimovic <milos.acimovic995@gmail.com>
2025-08-28 01:32:50 +0200

    Upgrade provider version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,126 +1,126 @@
-resource "azurerm_resource_group" "resource_group" {
-  name     = var.resource_group_name
-  location = var.location
-}
-#########################
-# Network configuration #
-#########################
-resource "azurerm_virtual_network" "network" {
-  name                = "network-keycloak"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-keycloak"
-  resource_group_name  = azurerm_resource_group.resource_group.name
-  virtual_network_name = azurerm_virtual_network.network.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "publicip-keycloak"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_network_interface" "network_interface" {
-  name                = "network-interface-keycloak"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
-  }
-}
-
-resource "azurerm_network_security_group" "security_group" {
-  name                = "security-group-keycloak"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Keycloak"
-    priority                   = 1003
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "8080"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
-  network_interface_id      = azurerm_network_interface.network_interface.id
-  network_security_group_id = azurerm_network_security_group.security_group.id
-}
-
-###################
-# Virtual machine #
-###################
-resource "azurerm_linux_virtual_machine" "virtual_machine" {
-  name                = "virtual-machine"
-  resource_group_name = azurerm_resource_group.resource_group.name
-  location            = azurerm_resource_group.resource_group.location
-  size                = "Standard_B1s"
-  admin_username      = var.username
-
-  network_interface_ids = [
-    azurerm_network_interface.network_interface.id
-  ]
-
-  admin_ssh_key {
-    username   = var.username
-    public_key = file(var.ssh_public_key)
-  }
-
-  os_disk {
-    name                 = "osdisk-keycloak"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
-}
-
-output "vm_public_ip" {
-  description = "Public IP address"
-  value       = azurerm_public_ip.public_ip.ip_address
-}
+# resource "azurerm_resource_group" "resource_group" {
+#   name     = var.resource_group_name
+#   location = var.location
+# }
+# #########################
+# # Network configuration #
+# #########################
+# resource "azurerm_virtual_network" "network" {
+#   name                = "network-keycloak"
+#   location            = azurerm_resource_group.resource_group.location
+#   resource_group_name = azurerm_resource_group.resource_group.name
+#   address_space       = ["10.0.0.0/16"]
+# }
+#
+# resource "azurerm_subnet" "subnet" {
+#   name                 = "subnet-keycloak"
+#   resource_group_name  = azurerm_resource_group.resource_group.name
+#   virtual_network_name = azurerm_virtual_network.network.name
+#   address_prefixes     = ["10.0.1.0/24"]
+# }
+#
+# resource "azurerm_public_ip" "public_ip" {
+#   name                = "publicip-keycloak"
+#   location            = azurerm_resource_group.resource_group.location
+#   resource_group_name = azurerm_resource_group.resource_group.name
+#   allocation_method   = "Static"
+#   sku                 = "Standard"
+# }
+#
+# resource "azurerm_network_interface" "network_interface" {
+#   name                = "network-interface-keycloak"
+#   location            = azurerm_resource_group.resource_group.location
+#   resource_group_name = azurerm_resource_group.resource_group.name
+#
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.subnet.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.public_ip.id
+#   }
+# }
+#
+# resource "azurerm_network_security_group" "security_group" {
+#   name                = "security-group-keycloak"
+#   location            = azurerm_resource_group.resource_group.location
+#   resource_group_name = azurerm_resource_group.resource_group.name
+#
+#   security_rule {
+#     name                       = "SSH"
+#     priority                   = 1001
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "22"
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
+#
+#   security_rule {
+#     name                       = "HTTP"
+#     priority                   = 1002
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "80"
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
+#
+#   security_rule {
+#     name                       = "Keycloak"
+#     priority                   = 1003
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "8080"
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
+# }
+#
+# resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
+#   network_interface_id      = azurerm_network_interface.network_interface.id
+#   network_security_group_id = azurerm_network_security_group.security_group.id
+# }
+#
+# ###################
+# # Virtual machine #
+# ###################
+# resource "azurerm_linux_virtual_machine" "virtual_machine" {
+#   name                = "virtual-machine"
+#   resource_group_name = azurerm_resource_group.resource_group.name
+#   location            = azurerm_resource_group.resource_group.location
+#   size                = "Standard_B1s"
+#   admin_username      = var.username
+#
+#   network_interface_ids = [
+#     azurerm_network_interface.network_interface.id
+#   ]
+#
+#   admin_ssh_key {
+#     username   = var.username
+#     public_key = file(var.ssh_public_key)
+#   }
+#
+#   os_disk {
+#     name                 = "osdisk-keycloak"
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+#
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-focal"
+#     sku       = "20_04-lts"
+#     version   = "latest"
+#   }
+# }
+#
+# output "vm_public_ip" {
+#   description = "Public IP address"
+#   value       = azurerm_public_ip.public_ip.ip_address
+# }
